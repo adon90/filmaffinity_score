358e4726db214a3dd52ceeb5639d420f2b790832
root <root@localhost.localdomain>
2016-05-13 23:38:24 +0100

    indent

--- scores.js
+++ scores.js
@@ -1,24 +1,24 @@
     var peliculas = $("div.movie-card-21");
     var i = 0;
     for (i; i < peliculas.length; i++){
-	peli_url = peliculas[i].querySelector("a").href;
-    $.ajax({
-      url : peli_url,
-      type : "get",
-      async: false,
-      success : function(data) {
-        score_rgx = /content="(\d(\.\d)?)"/.exec(data);
-        score = score_rgx === null ? 'TBD' : score_rgx[1];
-        info = peliculas[i].querySelector("div.mc-year-country");
-        node = document.createElement("SPAN");
-        text = document.createTextNode(" " + score);
-        node.appendChild(text);
-        node.style.color = "red";
-        node.style.fontSize = "25pt";
-        info.appendChild(node);
-      },
-      error: function() {
-         connectionError();
-      }
-   });
+		peli_url = peliculas[i].querySelector("a").href;
+	    $.ajax({
+	      url : peli_url,
+	      type : "get",
+	      async: false,
+	      success : function(data) {
+	        score_rgx = /content="(\d(\.\d)?)"/.exec(data);
+	        score = score_rgx === null ? 'TBD' : score_rgx[1];
+	        info = peliculas[i].querySelector("div.mc-year-country");
+	        node = document.createElement("SPAN");
+	        text = document.createTextNode(" " + score);
+	        node.appendChild(text);
+	        node.style.color = "red";
+	        node.style.fontSize = "25pt";
+	        info.appendChild(node);
+	      },
+	      error: function() {
+	         connectionError();
+	      }
+	   });
 }
